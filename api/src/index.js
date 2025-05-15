38f15d8f9e366e3d384061e4ad1771ade1d271df
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-05-15 12:34:26 +0200

    Connected with database, created routers

--- api/src/index.js
+++ api/src/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import knex from "./database_client.js";
 import nestedRouter from "./routers/nested.js";
+import { StatusCodes } from "http-status-codes";
 
 const app = express();
 app.use(cors());
@@ -13,12 +14,89 @@ const apiRouter = express.Router();
 
 // You can delete this route once you add your own routes
 apiRouter.get("/", async (req, res) => {
-  const SHOW_TABLES_QUERY =
-    process.env.DB_CLIENT === "pg"
-      ? "SELECT * FROM pg_catalog.pg_tables;"
-      : "SHOW TABLES;";
-  const tables = await knex.raw(SHOW_TABLES_QUERY);
-  res.json({ tables });
+  res.send(
+    "API is working!, try to access /api/future-meals, /api/past-meals, /api/all-meals, /api/first-meal, /api/last-meal"
+  );
+});
+
+//Route to get all meals in the future relative  to the current time
+apiRouter.get("/future-meals", async (req, res) => {
+  try {
+    const futureMeals = await knex("meal")
+      .select("*")
+      .where(`when`, ">", knex.fn.now());
+    console.log(futureMeals);
+    res.status(StatusCodes.OK).json(futureMeals);
+  } catch (error) {
+    console.error("Error fetching future meals:", error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal server error" });
+  }
+});
+// Router to get  all meals in the past (relative to the when datetime)
+apiRouter.get("/past-meals", async (req, res) => {
+  try {
+    const pastMeals = await knex("meal")
+      .select("*")
+      .where(`when`, "<", knex.fn.now());
+    console.log(pastMeals);
+    res.status(StatusCodes.OK).json(pastMeals);
+  } catch (error) {
+    console.error("Error fetching past meals:", error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ error: "Internal server error" });
+  }
+});
+
+//router to get all meals sorted by ID
+apiRouter.get("/all-meals", async (req, res) => {
+  try {
+    const allMeals = await knex("meal").select("*").orderBy("id", "DESC");
+
+    res.status(StatusCodes.OK).json(allMeals);
+    console.log(allMeals);
+  } catch (error) {
+    console.log("Error fetching all meals:", error);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "Internal server error",
+    });
+  }
+});
+
+// router to get first meal (meaning with the minimum id)
+apiRouter.get("/first-meal", async (req, res) => {
+  try {
+    const firstMeal = await knex("meal")
+      .select("*")
+      .orderBy("id", "ASC")
+      .limit(1);
+    res.status(StatusCodes.OK).json(firstMeal);
+    console.log(firstMeal);
+  } catch (err) {
+    res.satatus(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "Internal server error",
+    });
+    console.log("Error fetching first meal:", err);
+  }
+});
+
+// router to get last meal (meaning with the maximum id)
+apiRouter.get("/last-meal", async (req, res) => {
+  try {
+    const lastMeal = await knex("meal")
+      .select("*")
+      .orderBy("id", "DESC")
+      .limit(1);
+    res.status(StatusCodes.OK).json(lastMeal);
+    console.log(lastMeal);
+  } catch (err) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      error: "Internal server error",
+    });
+    console.log("Error fetching last meal:", err);
+  }
 });
 
 // This nested router example can also be replaced with your own sub-router
