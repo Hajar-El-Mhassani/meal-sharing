f9604b52f5dcf527795999c4fb36962e8109f19c
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-06-25 14:26:20 +0200

    Implemented MUI for styling

--- app-next/components/MealList/Meal.jsx
+++ app-next/components/MealList/Meal.jsx
@@ -1,31 +1,52 @@
-// components/MealCard.jsx
-import styles from "./MealCard.module.css";
+import { Card, CardMedia, Box, CardContent, Typography } from "@mui/material";
 
-const MealCard = (meal) => {
+const MealCard = ({ meal }) => {
   return (
-    <article className={styles.meal}>
-      <img
-        src="/meals/image.png"
+    <Card
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 1,
+        borderRadius: 0,
+        height: "100%",
+        maxWidth: 300,
+        margin: "0 auto",
+      }}
+    >
+      <CardMedia
+        component="img"
+        height="194"
+        image="/meals/image.png"
         alt={meal.title}
-        className={styles.mealImage}
       />
-      <h3>{meal.title}</h3>
-      <p>{meal.description}</p>
-      <p>
-        <strong>Date: </strong>
-        {new Date(meal.when).toLocaleDateString("en-EN")}
-      </p>
-      <p>
-        <strong>Location: </strong>
-        {meal.location}
-      </p>
-      <p>
-        <strong>Max reservation: </strong> {meal.max_reservation}
-      </p>
-      <p>
-        <strong>Price:</strong> {meal.price}
-      </p>
-    </article>
+      <CardContent sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+        <Typography variant="h6">{meal.title}</Typography>
+        <Typography variant="body2" color="text.secondary">
+          {meal.description}
+        </Typography>
+        <Typography variant="body2">
+          Date:{" "}
+          <Box component="span" color="text.secondary">
+            {new Date(meal.when).toLocaleDateString("en-EN")}
+          </Box>
+        </Typography>
+        <Typography variant="body2">
+          Location:{" "}
+          <Box component="span" color="text.secondary">
+            {meal.location}
+          </Box>
+        </Typography>
+        <Typography variant="body2">
+          Max reservation:{" "}
+          <Box component="span" color="text.secondary">
+            {meal.max_reservation}
+          </Box>
+        </Typography>
+        <Typography variant="subtitle1" color="#27ae60">
+          <strong> Price:</strong> ${meal.price}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
--- app-next/components/MealList/MealList.jsx
+++ app-next/components/MealList/MealList.jsx
@@ -1,12 +1,18 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import styles from "./MealList.module.css";
+import { Container, Box, Grid, Typography } from "@mui/material";
 import MealCard from "./Meal";
 import api from "../../utils/api";
 const MealList = () => {
   const [meals, setMeals] = useState([]);
 
   useEffect(() => {
+    /*************  ✨ Windsurf Command ⭐  *************/
+    /**
+   * Fetches meals from the API and updates the state with the response data.
+   *
+
+/*******  5f549e22-fea6-40d6-9ec4-9e566f67b20b  *******/
     const fetchMeals = async () => {
       try {
         const response = await fetch(api("/meals"));
@@ -22,14 +28,33 @@ const MealList = () => {
   }, []);
 
   return (
-    <section className={styles.sectionMeal}>
-      <h2>Meals</h2>
-      <div className={styles.meals}>
-        {meals.map((meal) => (
-          <MealCard {...meal} key={meal.id} />
-        ))}
-      </div>
-    </section>
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          margin: "0 auto",
+          padding: "2rem",
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            textAlign: "center",
+            marginBottom: "2rem",
+            fontSize: "2rem",
+            color: "#333",
+          }}
+        >
+          Meals
+        </Typography>
+        <Grid container spacing={4} justifyContent="center">
+          {meals.map((meal) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} key={meal.id}>
+              <MealCard meal={meal} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
