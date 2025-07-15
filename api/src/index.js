e38041bd7c299618179f4e5029802b8235c01c27
Hajar-El-Mhassani <hajarelmhassani14@gmail.com>
2025-07-15 20:51:32 +0200

    change extension of index.mjs to index.js

--- api/src/index.js
+++ api/src/index.js
@@ -33,6 +33,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 apiRouter.get("/future-meals", async (req, res) => {
   try {
     const futureMeals = await knex("meal")
